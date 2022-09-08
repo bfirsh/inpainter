4094f168285235d31f3f784a5da0a46065d8b312
Ben Firshman <ben@firshman.co.uk>
2022-09-08 07:38:50 -0700

    Add title

--- pages/paint.js
+++ pages/paint.js
@@ -81,7 +81,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Inpainter</title>
+        <title>Inpainting with Stable Diffusion &amp; Replicate</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
